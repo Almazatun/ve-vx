505327a965f4814917181d561c25448a0bbec5fa
Almaz <atunstyle@gmail.com>
2020-12-29 22:24:14 +0300

    modify files and add new component *Imported Tailwindcss styles in the main.js file. *Added Routers component. *Deleted export default property from index.ts file in the store folder.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container flex flex-col bg-green-100 h-screen w-screen items-center text-center">
+    <Routers/>
+    <router-view/>
   </div>
-  <router-view />
+
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Routers from "@/components/Routers";
 
-#nav {
-  padding: 30px;
+export default {
+  components: {
+    Routers: Routers
+  }
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
