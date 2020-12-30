da8f2d05759ee2ff7be43808baec8c8a03a96ad2
Almaz <atunstyle@gmail.com>
2020-12-31 00:11:59 +0300

    add mutations actions of the rootState

---
+++ src/store/mutations.ts
@@ -0,0 +1,24 @@
+import {MutationTree} from "vuex";
+import {StoreState, StatusT} from "@/store/index";
+
+//Enums
+export enum STORE_MUTATIONS {
+    SET_STATUS = 'SET_STATUS',
+    SET_ERROR = 'SET_ERROR'
+}
+
+//Types
+export type MutationsT<S = StoreState> = {
+    [STORE_MUTATIONS.SET_STATUS](state: S, payload: StatusT): void
+    [STORE_MUTATIONS.SET_ERROR](state: S, payload: string): void
+}
+
+
+export const mutations: MutationTree<StoreState> & MutationsT = {
+    [STORE_MUTATIONS.SET_STATUS](state: StoreState, payload) {
+        state.status = payload
+    },
+    [STORE_MUTATIONS.SET_ERROR](state, payload) {
+        state.error = payload
+    }
+}
