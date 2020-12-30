79960bccc39c8e8a8780131f93557f7a6e6681f4
Almaz <atunstyle@gmail.com>
2020-12-31 00:27:20 +0300

    :label:modify and add types *Added types and imported key properties of the store.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,35 @@
-import { createStore } from "vuex";
+import {createStore} from "vuex";
+import {mutations} from "@/store/mutations";
+import {actions} from "@/store/actions";
+import {getters} from "@/store/getters";
 
-export const store =  createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+//Types
+export interface StoreState {
+    status: StatusT
+    error: string | null
+}
+
+export type StatusT = 'idle' | 'loading' | 'success' | 'failed'
+
+//Enums
+export enum STORE_STATUS {
+    IDLE = 'idle',
+    LOADING = 'loading',
+    SUCCESS = 'success',
+    FAILED = 'failed'
+}
+
+
+const state: StoreState = {
+    status: 'idle',
+    error: null,
+}
+
+export const store = createStore({
+    state: state,
+    mutations: mutations,
+    actions: actions,
+    getters: getters,
+    modules: {}
 });
+
