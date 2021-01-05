ed55624cbc3a6ca30068344205cdbf8dd84ec4c2
Almaz <atunstyle@gmail.com>
2021-01-05 17:25:38 +0300

    add new Todolist component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span>{{ todoListTitle }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "TodoList",
+  props: {
+    todoListTitle: {
+      type: String,
+      required: true
+    }
+  }
+})
+
+</script>
