5be5b957419c1c17465b7ae6bfa51e2c366396c1
Almaz <atunstyle@gmail.com>
2021-01-09 19:15:56 +0300

    add new actions to ability delete and add new tl *Added some types of new actions methods

--- src/store/modules/todo_lists/actions_todo_lists.ts
+++ src/store/modules/todo_lists/actions_todo_lists.ts
@@ -3,14 +3,15 @@ import {MutationsT, STORE_MUTATIONS} from "@/store/mutations";
 import {STORE_STATUS} from "@/store";
 import {API_TODO_LISTS} from "@/api/api";
 import {ActionContext, ActionTree} from "vuex";
-import {TodoListsStateT} from "@/store/modules/todo_lists/todo_lists";
+import {TodoListsStateT, TodoList} from "@/store/modules/todo_lists/todo_lists";
 import {RootState} from "@/confirm/types";
 
 //Enums
 export enum ACTIONS_TODO_LISTS {
     FETCH_TLS = 'FETCH_TLS',
     REMOVE_TL = 'REMOVE_TL',
-    CHANGE_TL_TITLE = 'CHANGE_TL_TITLE'
+    CHANGE_TL_TITLE = 'CHANGE_TL_TITLE',
+    CREATE_TL = 'CREATE_TL'
 }
 
 //Types
@@ -31,6 +32,14 @@ export interface ActionsTodoLists {
         {commit}: AugmentedTLSActionContext,
         payload: TodoListsStateT
     ): void
+    [ACTIONS_TODO_LISTS.CREATE_TL](
+        {commit}: AugmentedTLSActionContext,
+        payload: {title: string}
+    ):void
+    [ACTIONS_TODO_LISTS.REMOVE_TL](
+        {commit}: AugmentedTLSActionContext,
+        payload: { todoListId: string }
+    ):void
 }
 
 export const actionsTodoLists: ActionTree<TodoListsStateT, RootState> & ActionsTodoLists = {
@@ -40,6 +49,36 @@ export const actionsTodoLists: ActionTree<TodoListsStateT, RootState> & ActionsT
             const receiveTodoLists = await API_TODO_LISTS.getTodoLists()
             commit(MUTATIONS_TODO_LISTS.SET_TLS, {todoLists: receiveTodoLists})
             commit(STORE_MUTATIONS.SET_STATUS, STORE_STATUS.SUCCESS, {root: true})
+        } catch (error) {
+            if (error.messages.length) {
+                commit(STORE_MUTATIONS.SET_ERROR, error.message)
+            } else {
+                commit(STORE_MUTATIONS.SET_ERROR, 'Some error')
+            }
+        }
+    },
+    async[ACTIONS_TODO_LISTS.CREATE_TL]({commit}, payload){
+        const data: {title: string} = {title: payload.title}
+        try {
+            //disable
+            const receiveData = await API_TODO_LISTS.createTodoList(data)
+            const newData: {newTodoList: TodoList} = {newTodoList: {...receiveData.data.item, requestStatus: "idle"} }
+            commit(MUTATIONS_TODO_LISTS.ADD_TL, newData)
+            //un-disable
+        } catch (error) {
+            if (error.messages.length) {
+                commit(STORE_MUTATIONS.SET_ERROR, error.message)
+            } else {
+                commit(STORE_MUTATIONS.SET_ERROR, 'Some error')
+            }
+        }
+    },
+    async[ACTIONS_TODO_LISTS.REMOVE_TL]({commit}, payload){
+        try {
+            //disable
+            const receiveData = await API_TODO_LISTS.deleteTodoList(payload.todoListId)
+            commit(MUTATIONS_TODO_LISTS.REMOVE_TL, {todoListId: payload.todoListId})
+            //un-disable
         } catch (error) {
             if (error.messages.length) {
                 commit(STORE_MUTATIONS.SET_ERROR, error.message)
