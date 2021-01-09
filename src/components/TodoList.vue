62ee0ab8c8a82cb8da8c8eba4e6b436c54d0901d
Almaz <atunstyle@gmail.com>
2021-01-09 19:34:42 +0300

    improve css styles and add features of the component *Added toggle menu. The toggle menu allows to choice feature to modify the to-do list. *Improved css styles *The features to able to delete and add a new list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>
-    <span>{{ todoListTitle }}</span>
+  <div style="min-height: 250px" class="container rounded-lg lg:w-96 md:w-60 bg-white p-4 shadow-2xl relative">
+    <div class="flex flex-row justify-between ">
+      <span class="font-bold ">{{ todoListTitle }}</span>
+      <button v-if="!show" class="focus:outline-none p-1.5" @click="toggleMenu">
+        <img :src="editTodoListIcon" width="25" height="25" alt="edit logo">
+      </button>
+      <button v-else @click="toggleMenu" class="focus:outline-none p-1.5">
+        <img :src="closeMenuTodoListIcon" width="25" height="25" alt="edit logo">
+      </button>
+    </div>
+    <div v-if="show" class="flex flex-col absolute right-2 bg-gray-100 rounded-lg">
+      <span class="hover:bg-blue-200 p-4 rounded-t-lg cursor-pointer">Rename list</span>
+      <span @click="removeTodoList" class="hover:bg-red-100 p-4 rounded-b-lg cursor-pointer">Delete list</span>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {defineComponent, ref} from 'vue'
 
 export default defineComponent({
   name: "TodoList",
@@ -13,6 +25,41 @@ export default defineComponent({
     todoListTitle: {
       type: String,
       required: true
+    },
+    deleteTodoList: {
+      type: Function,
+      required: true
+    },
+    todoListId: {
+      type: String,
+      required: true
+    }
+  },
+  setup (props){
+    const editTodoListIcon = "https://cdn1.iconfinder.com/data/icons/jumpicon-basic-ui-glyph-1/32/-_Dot-More-Vertical-Menu-512.png"
+    const closeMenuTodoListIcon = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/OOjs_UI_icon_close.svg/480px-OOjs_UI_icon_close.svg.png"
+    const show = ref<boolean>(false)
+
+    function toggleMenu(){
+      show.value = !show.value
+    }
+
+    function removeTodoList(){
+      if (props.todoListId !== ''){
+        const should = window.confirm("Are you sure you want to permanently delete todo list ?")
+        if (should) {
+          props.deleteTodoList(props.todoListId)
+          toggleMenu()
+        }
+      }
+    }
+
+    return {
+      editTodoListIcon,
+      closeMenuTodoListIcon,
+      show,
+      toggleMenu,
+      removeTodoList
     }
   }
 })
