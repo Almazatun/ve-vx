7ef6c2cc54d44d97fe6082c68f540908410b106f
Almaz <atunstyle@gmail.com>
2021-01-12 08:08:18 +0300

    add action method *FETCH_I method. *The method takes one argument is todoListId make a request get items of the todo lists.

--- src/store/modules/todo_items/actions_todo_Items.ts
+++ src/store/modules/todo_items/actions_todo_Items.ts
@@ -1,3 +1,50 @@
-export const actionsItems = {
+import {MUTATIONS_TODO_ITEMS, MutationsTodoItems} from "@/store/modules/todo_items/mutations_todo_Items";
+import {MutationsT, STORE_MUTATIONS} from "@/store/mutations";
+import {ActionContext, ActionTree} from "vuex";
+import {RootState} from "@/confirm/types";
+import {ItemsStateT} from "@/store/modules/todo_items/todo_Items";
+import {STORE_STATUS} from "@/store";
+import {API_ITEMS} from "@/api/api";
 
+
+//Enums
+export enum ACTIONS_TODO_ITEMS {
+    FETCH_I = 'FETCH_I'
+}
+
+//Types
+type MergeMutationsT = MutationsTodoItems & MutationsT
+
+type AugmentedActionContext = {
+    commit<K extends keyof MergeMutationsT>(
+        key: K,
+        payload: Parameters<MergeMutationsT[K]>[1],
+        /* eslint-disable */
+        {}?
+    ): ReturnType<MergeMutationsT[K]>
+} & Omit<ActionContext<ItemsStateT, RootState>, 'commit'>
+
+export interface ActionsTodoItems {
+    [ACTIONS_TODO_ITEMS.FETCH_I](
+        {commit}: AugmentedActionContext,
+        payload: {type: string, todoListId: string}
+    ): void
+}
+
+export const actionsItems: ActionTree<ItemsStateT, RootState> & ActionsTodoItems = {
+    async [ACTIONS_TODO_ITEMS.FETCH_I]({commit}, payload) {
+        const {todoListId} = payload
+        try {
+            commit(STORE_MUTATIONS.SET_STATUS, STORE_STATUS.LOADING, {root: true})
+            const receiveData = await API_ITEMS.getItemsOfTheList(todoListId)
+            commit(MUTATIONS_TODO_ITEMS.SET_ITEMS, {todoListId, items: receiveData.items})
+            commit(STORE_MUTATIONS.SET_STATUS, STORE_STATUS.SUCCESS, {root: true})
+        } catch (error) {
+            if (error.messages.length) {
+                commit(STORE_MUTATIONS.SET_ERROR, error.message)
+            } else {
+                commit(STORE_MUTATIONS.SET_ERROR, 'Some error')
+            }
+        }
+    }
 }
