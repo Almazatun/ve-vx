4922b14f6eb844a8a5d1ea416a608439289f07e4
Almaz <atunstyle@gmail.com>
2021-01-12 08:00:10 +0300

    :label:add mutation method *SET_ITEMS method *This method sets all receive data from server to Item module state. *Added some type

--- src/store/modules/todo_items/mutations_todo_Items.ts
+++ src/store/modules/todo_items/mutations_todo_Items.ts
@@ -1,3 +1,22 @@
-export const mutationsItems = {
+import {ItemsStateT} from "@/store/modules/todo_items/todo_Items";
+import {MutationTree} from "vuex";
+import {Item} from "@/api/api";
 
+
+//Enums
+export enum MUTATIONS_TODO_ITEMS {
+    SET_ITEMS = 'SET_ITEMS',
+}
+
+//Types
+export type MutationsTodoItems<I = ItemsStateT> = {
+    [MUTATIONS_TODO_ITEMS.SET_ITEMS](state: I, payload: {todoListId: string, items: Array<Item>}): void
+}
+
+export const mutationsItems: MutationTree<ItemsStateT> & MutationsTodoItems = {
+    [MUTATIONS_TODO_ITEMS.SET_ITEMS](state, payload){
+        state.items[payload.todoListId] = payload.items.map(i => {
+            return {...i, requestStatus: 'idle'}
+        })
+    },
 }
