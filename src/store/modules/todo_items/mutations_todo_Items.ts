ece521ca769b819b61e7f77e764620a62ad33282
Almaz <atunstyle@gmail.com>
2021-01-12 15:16:05 +0300

    :building_construction:add new mutations of the Items module *Ability to change the status of a specific item. *Ability to rename the title of a specific item. *Ability to delete a specific item. *Ability to change request status of a specific item.

--- src/store/modules/todo_items/mutations_todo_Items.ts
+++ src/store/modules/todo_items/mutations_todo_Items.ts
@@ -1,16 +1,35 @@
 import {ItemsStateT} from "@/store/modules/todo_items/todo_Items";
 import {MutationTree} from "vuex";
-import {Item} from "@/api/api";
+import {Item, ItemStatuses} from "@/api/api";
+import {StatusT} from "@/store";
 
 
 //Enums
 export enum MUTATIONS_TODO_ITEMS {
     SET_ITEMS = 'SET_ITEMS',
+    CHANGE_STATUS_I = 'CHANGE_STATUS_I',
+    DELETE_I = 'DELETE_I',
+    RENAME_TITLE_I = 'RENAME_TITLE_I',
+    CHANGE_REQUEST_STATUS_I = 'CHANGE_REQUEST_STATUS_I'
 }
 
 //Types
 export type MutationsTodoItems<I = ItemsStateT> = {
-    [MUTATIONS_TODO_ITEMS.SET_ITEMS](state: I, payload: {todoListId: string, items: Array<Item>}): void
+    [MUTATIONS_TODO_ITEMS.SET_ITEMS](state: I, payload: {
+        todoListId: string, items: Array<Item>
+    }): void
+    [MUTATIONS_TODO_ITEMS.CHANGE_STATUS_I](state: I, payload: {
+        todoListId: string, itemId: string, status: ItemStatuses
+    }): void
+    [MUTATIONS_TODO_ITEMS.RENAME_TITLE_I](state: I, payload: {
+        todoListId: string, itemId: string, title: string
+    }): void
+    [MUTATIONS_TODO_ITEMS.DELETE_I](state: I, payload: {
+        todoListId: string, itemId: string
+    }): void
+    [MUTATIONS_TODO_ITEMS.CHANGE_REQUEST_STATUS_I](state: I, payload: {
+        todoListId: string, itemId: string, requestStatus: StatusT
+    }): void
 }
 
 export const mutationsItems: MutationTree<ItemsStateT> & MutationsTodoItems = {
@@ -19,4 +38,29 @@ export const mutationsItems: MutationTree<ItemsStateT> & MutationsTodoItems = {
             return {...i, requestStatus: 'idle'}
         })
     },
+    [MUTATIONS_TODO_ITEMS.CHANGE_STATUS_I](state, payload){
+        state.items[payload.todoListId] = [...state.items[payload.todoListId].map(i => {
+         return i.id !== payload.itemId ? i : {...i, status: payload.status}
+        })]
+    },
+    [MUTATIONS_TODO_ITEMS.RENAME_TITLE_I](state, payload){
+        state.items[payload.todoListId] = [...state.items[payload.todoListId].map(i => {
+            return i.id !== payload.itemId ? i : {...i, title: payload.title}
+        })]
+    },
+    [MUTATIONS_TODO_ITEMS.DELETE_I](state, payload){
+        state.items[payload.todoListId].forEach((i, index, object) => {
+            if (i.id === payload.itemId) {
+                object.splice(index, 1)
+            }
+        })
+    },
+    [MUTATIONS_TODO_ITEMS.CHANGE_REQUEST_STATUS_I](state, payload){
+        state.items[payload.todoListId] = [
+            ...state.items[payload.todoListId].map(i => {
+                return i.id !== payload.itemId ? i : {...i, requestStatus: payload.requestStatus}
+            })
+        ]
+    }
+
 }
