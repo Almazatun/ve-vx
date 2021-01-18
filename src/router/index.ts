70b4783cab93023da3d264b4f657cbd0d22d5844
Almaz <atunstyle@gmail.com>
2021-01-18 06:57:34 +0300

    :passport_control:include navigation guards *Unless the user is not authorized, will be redirected to the Login page.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,38 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import {createRouter, createWebHashHistory, RouteRecordRaw} from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import {store} from "@/store";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+        beforeEnter: (to, from, next) => {
+            if (store.getters['AUTH/isAuth']) {
+                next()
+                return
+            }
+            next('/login')
+        }
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
+        beforeEnter: (to, from, next) => {
+            if (!store.getters['AUTH/isAuth']) {
+                next()
+                return
+            }
+            next('/')
+        },
+    },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 });
 
 export default router;
